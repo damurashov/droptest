4a1e993c49cce15b64ade66167262153a7076df9
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-18 18:23:48 +0300

    [stub:main] Impl | Test stubs

--- stub/main.cpp
+++ stub/main.cpp
@@ -3,9 +3,15 @@
 
 #include <OhDebug.hpp>
 
+OHDEBUG_TEST("Test name")
+{
+	OHDEBUG("Trace", "Traced");
+}
+
 int main(void)
 {
 	OHDEBUG("Trace", "EXECUTABLE_NAME_STUB");
+	OHDEBUG_RUN_TESTS();
 
 	return 0;
 }
